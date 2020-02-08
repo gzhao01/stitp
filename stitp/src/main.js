cab28210e922c355324732360451c42beb71bbb5
gzhao01 <1079756007@qq.com>
2020-02-09 04:08:12 +0800

    personal info

--- stitp/src/main.js
+++ stitp/src/main.js
@@ -1,15 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import 'styles/font/iconfont.css'
+import 'styles/reset.css'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  // render: h => h(App)
 })
--- stitp/src/router/index.js
+++ stitp/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import PersonalInfo from '@/pages/personalInfo/PersonalInfo.vue'
+import Login from '@/pages/Login.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'PersonalInfo',
+      component: PersonalInfo
+    },
+    {
+      path: '/login',
+      name: "Login",
+      component: Login
     }
   ]
 })
